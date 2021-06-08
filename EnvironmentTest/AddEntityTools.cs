712303d84d643aedea7fc4ab2196d9e3af891048
MrJson13 <Administrator@PC201909171703>
2021-06-08 15:26:09 +0800

    init

---
+++ EnvironmentTest/AddEntityTools.cs
@@ -0,0 +1,310 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvironmentTest
+{
+    public static partial class AddEntityTools
+    {
+        #region 添加图形封装
+        /// <summary>
+        /// 添加单个图形文件
+        /// </summary>
+        /// <param name="db">图形数据库</param>
+        /// <param name="ent">图形对象</param>
+        /// <returns></returns>
+        public static ObjectId AddEntityToModeSpace(this Database db, Entity ent)
+        {
+            ObjectId entId = ObjectId.Null;
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                //以读的方式打开块表
+                BlockTable bt = (BlockTable)trans.GetObject(db.BlockTableId, OpenMode.ForRead);
+                //以写打开块表记录
+                BlockTableRecord btr = (BlockTableRecord)trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
+                //加到块表记录
+                btr.AppendEntity(ent);
+                //更新数据
+                trans.AddNewlyCreatedDBObject(ent, true);
+                //事务提交
+                trans.Commit();
+            }
+            return entId;
+        }
+        /// <summary>
+        /// 添加多个图形文件
+        /// </summary>
+        /// <param name="db">图形数据库</param>
+        /// <param name="ent">图形对象</param>
+        /// <returns></returns>
+        public static ObjectId[] AddEntityToModeSpace(this Database db,params Entity[] ent)
+        {
+            ObjectId[] entId = new ObjectId[ent.Length];
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                //以读的方式打开块表
+                BlockTable bt = (BlockTable)trans.GetObject(db.BlockTableId, OpenMode.ForRead);
+                //以写打开块表记录
+                BlockTableRecord btr = (BlockTableRecord)trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
+                for(int i =0; i < ent.Length; i++)
+                {
+                    //加到块表记录
+                    entId[i] = btr.AppendEntity(ent[i]);
+                    //更新数据
+                    trans.AddNewlyCreatedDBObject(ent[i], true);
+                }               
+                //事务提交
+                trans.Commit();
+            }
+            return entId;
+        }
+        #endregion
+        #region 直线绘制封装
+        /// <summary>
+        /// 根据起始坐标绘制直线
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="stratPoint">起点坐标</param>
+        /// <param name="endPoint">始点坐标</param>
+        /// <returns></returns>
+        public static ObjectId AddLineToModeSpace(this Database db, Point3d startPoint,Point3d endPoint)
+        {
+            return db.AddEntityToModeSpace(new Line(startPoint, endPoint));
+        }
+        /// <summary>
+        /// 起点坐标，长度，弧度
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="startPoint"></param>
+        /// <param name="length"></param>
+        /// <param name="degree"></param>
+        /// <returns></returns>
+        public static ObjectId AddLineToModeSpace(this Database db, Point3d startPoint, Double length,Double degree)
+        {
+            //计算始点坐标
+            double X = startPoint.X + length * Math.Cos(degree.DegreeToAngle());
+            double Y = startPoint.Y + length * Math.Sin(degree.DegreeToAngle());
+            Point3d endPoint = new Point3d(X, Y, 0);
+            return db.AddEntityToModeSpace(new Line(startPoint, endPoint));
+        }
+        #endregion
+        #region 圆弧绘制封装
+        /// <summary>
+        /// 绘制圆弧
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="center">中心</param>
+        /// <param name="radius">半径</param>
+        /// <param name="startDegree">起始角度</param>
+        /// <param name="endDegree">起始角度</param>
+        /// <returns></returns>
+        public static ObjectId AddArcToModeSpace(this Database db,Point3d center,double radius,double startDegree,double endDegree)
+        {
+            return db.AddEntityToModeSpace(new Arc(center, radius, startDegree.DegreeToAngle(), endDegree.DegreeToAngle()));
+        }
+        /// <summary>
+        /// 三点绘制圆弧
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="startPoint"></param>
+        /// <param name="pointOnArc"></param>
+        /// <param name="endPoint"></param>
+        /// <returns></returns>
+        public static ObjectId AddArcToModeSpace(this Database db,Point3d startPoint,Point3d pointOnArc,Point3d endPoint)
+        {
+            //判断是否在同一直线上
+            if (startPoint.IsOnOneLine(pointOnArc, endPoint))
+            {
+                return ObjectId.Null;
+            }
+            //创建几何对象
+            CircularArc3d cArc = new CircularArc3d(startPoint,pointOnArc,endPoint);
+            //通过几何对象获取属性
+            double radius = cArc.Radius;
+            //创建圆弧对象
+            Arc arc = new Arc(cArc.Center, cArc.Radius, cArc.Center.GetAngleToXAxis(startPoint), cArc.Center.GetAngleToXAxis(endPoint));
+            return db.AddEntityToModeSpace(arc);
+        }
+        #endregion
+        #region 圆的封装
+        /// <summary>
+        /// 绘制圆
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="center">圆心</param>
+        /// <param name="radius">半径</param>
+        /// <returns></returns>
+        public static ObjectId AddCircleToModeSpace(this Database db,Point3d center,double radius)
+        {
+            return db.AddEntityToModeSpace(new Circle((center), new Vector3d(0, 0, 1), radius));
+        }
+        /// <summary>
+        /// 两点画圆
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
+        /// <returns></returns>
+        public static ObjectId AddCircleToModeSpace(this Database db,Point3d point1,Point3d point2)
+        {
+            //获取两点之间的中心
+            Point3d center = point1.GetCenterPointBetweenTwoPoint(point2);
+            //获取半径
+            double radius = point1.GetDistanceBetweenTwoPoint(center);
+            return db.AddCircleToModeSpace(center, radius);
+        }
+        /// <summary>
+        /// 三点画圆
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
+        /// <param name="point3"></param>
+        /// <returns></returns>
+        public static ObjectId AddCircleToModeSpace(this Database db, Point3d point1, Point3d point2, Point3d point3)
+        {
+            //判断三点是否在同一直线上
+            if (point1.IsOnOneLine(point2, point3))
+            {
+                return ObjectId.Null;
+            }
+            //声明几何对象Circle3d
+            CircularArc3d cArc = new CircularArc3d(point1, point2, point3);
+            return db.AddCircleToModeSpace(cArc.Center, cArc.Radius);
+        }
+        #endregion
+        #region 多线段封装
+        /// <summary>
+        /// 绘制多线段
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="isClosed">是否闭合</param>
+        /// <param name="contentWidth">线宽</param>
+        /// <param name="vertices">多线段的顶点，可变参数</param>
+        /// <returns></returns>
+        public static ObjectId AddPolyLineToModeSpace(this Database db,bool isClosed,double contentWidth,params Point2d[] vertices)
+        {
+            //顶点个数小于2无法绘制
+            if (vertices.Length < 2)
+            {
+                return ObjectId.Null;
+            }
+            //声明一个多线段对象
+            Polyline pline = new Polyline();
+            //添加多线段顶点
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                pline.AddVertexAt(i, vertices[i], 0, 0, 0);
+            }
+            if (isClosed)
+            {
+                pline.Closed = true;
+            }
+            pline.ConstantWidth = contentWidth;
+            return db.AddEntityToModeSpace(pline);
+        }
+        #endregion
+        #region 矩形封装
+        /// <summary>
+        /// 绘制矩形
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="point1">起点</param>
+        /// <param name="point2">对角点</param>
+        /// <returns></returns>
+        public static ObjectId AddRectToModeSpace(this Database db, Point2d point1, Point2d point2)
+        {
+            //声明多线段
+            Polyline pline = new Polyline();
+            //计算矩形的四个顶点坐标
+            Point2d p1 = new Point2d(Math.Min(point1.X, point2.X), Math.Min(point1.Y, point2.Y));
+            Point2d p2 = new Point2d(Math.Max(point1.X, point2.X), Math.Min(point1.Y, point2.Y));
+            Point2d p3 = new Point2d(Math.Max(point1.X, point2.X), Math.Max(point1.Y, point2.Y));
+            Point2d p4 = new Point2d(Math.Min(point1.X, point2.X), Math.Max(point1.Y, point2.Y));
+            //添加多线段顶点
+            pline.AddVertexAt(0, p1, 0, 0,0);
+            pline.AddVertexAt(0, p2, 0, 0, 0);
+            pline.AddVertexAt(0, p3, 0, 0, 0);
+            pline.AddVertexAt(0, p4, 0, 0, 0);
+            pline.Closed = true;//闭合
+            return db.AddEntityToModeSpace(pline);
+        }
+        #endregion
+        #region 正多边形封装
+        /// <summary>
+        /// 绘制正多边形
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="center">正多边形所在圆圆心</param>
+        /// <param name="radius">所在圆半径</param>
+        /// <param name="sideNum">变数</param>
+        /// <param name="startDegree">起始角度</param>
+        /// <returns></returns>
+        public static ObjectId AddPolygonToModeSpace(this Database db,Point2d center,double radius,int sideNum,double startDegree)
+        {
+            //声明一个多线段对象
+            Polyline pline = new Polyline();
+            //判断变数是否符合
+            if (sideNum < 3)
+            {
+                return ObjectId.Null;
+            }
+            Point2d[] point = new Point2d[sideNum];
+            double angle = startDegree.DegreeToAngle();
+            //计算每个顶点坐标
+            for (int i = 0; i < sideNum; i++)
+            {
+                point[i] = new Point2d(center.X + radius * Math.Cos(angle), center.Y + radius * Math.Sin(angle));
+                pline.AddVertexAt(i, point[i], 0, 0, 0);
+                angle += Math.PI * 2 / sideNum;
+            }
+            pline.Closed = true;//闭合
+            return db.AddEntityToModeSpace(pline);
+        }
+        #endregion
+        #region 椭圆封装
+        /// <summary>
+        /// 两点绘制椭圆
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
+        /// <returns></returns>
+        public static ObjectId AddEllipseToModeSpace(this Database db, Point3d point1, Point3d point2)
+        {
+            //椭圆圆心
+            Point3d center = point1.GetCenterPointBetweenTwoPoint(point2);
+
+            double ratio = Math.Abs((point1.Y - point2.Y) / (point1.X - point2.X));
+            Vector3d majorVector = new Vector3d(Math.Abs((point1.X - point2.X)) / 2, 0, 0);
+            //声明椭圆对象
+            Ellipse elli = new Ellipse(center, Vector3d.ZAxis, majorVector, ratio, 0, 2 * Math.PI);
+            return db.AddEntityToModeSpace(elli);
+        }
+        /// <summary>
+        /// 三点绘制椭圆
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
+        /// <param name="shortRadius"></param>
+        /// <returns></returns>
+        public static ObjectId AddEllipseToModeSpace(this Database db, Point3d point1, Point3d point2,double shortRadius)
+        {
+            //椭圆圆心
+            Point3d center = point1.GetCenterPointBetweenTwoPoint(point2);
+            //短长轴比例
+            double ratio = 2 * shortRadius / point1.GetDistanceBetweenTwoPoint(point2);
+            //长轴向量
+            Vector3d majorVector = point2.GetVectorTo(center);
+            //声明椭圆对象
+            Ellipse elli = new Ellipse(center, Vector3d.ZAxis, majorVector, ratio, 0, 2 * Math.PI);
+            return db.AddEntityToModeSpace(elli);
+        }
+        #endregion
+    }
+}
---
+++ EnvironmentTest/Class1.cs
@@ -0,0 +1,159 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvironmentTest
+{
+    public class Class1
+    {
+        #region Day1
+        /// <summary>
+        /// 测试环境
+        /// </summary>
+        [CommandMethod("TestEnv")] // 添加命令标识符
+        public void TestEnv()
+        {
+            // 声明命令行对象
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            // 向命令行输出一段文字
+            ed.WriteMessage("CAD二次开发笔记（1）：环境测试！");
+        }
+        #endregion
+        #region Day2
+        /// <summary>
+        /// 绘线
+        /// </summary>
+        [CommandMethod("DrawLines")]
+        public void DrawLines()
+        {
+            //声明一个直线对象
+            Line lien1 = new Line();
+            //声明两个坐标点对象
+            Point3d startPoint = new Point3d(100, 100, 0);
+            Point3d endPoint = new Point3d(200, 200, 0);
+            //设置属性
+            lien1.StartPoint = startPoint;
+            lien1.EndPoint = endPoint;
+            //声明数据库对象
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            //开启事务处理
+            using(Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                //以读的方式打开块表
+                BlockTable bt = (BlockTable)trans.GetObject(db.BlockTableId, OpenMode.ForRead);
+                //以写打开块表记录
+                BlockTableRecord btr = (BlockTableRecord)trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
+                //加直线到块表记录
+                btr.AppendEntity(lien1);
+                //更新数据
+                trans.AddNewlyCreatedDBObject(lien1, true);
+                //事务提交
+                trans.Commit();
+            }
+        }
+        #endregion
+        #region Day3
+        [CommandMethod("TestLine")]
+        public void TestLine()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            Line line1 = new Line(new Point3d(100, 100, 0), new Point3d(200, 100, 0));
+            //调用封装函数,添加图形文件
+            //db.AddEntityToModeSpace(line1);
+            Line line2 = new Line(new Point3d(200, 100, 0), new Point3d(200, 200, 0));
+            Line line3 = new Line(new Point3d(200, 200, 0), new Point3d(100, 100, 0));
+            //调用封装函数,添加多个图形文件
+            db.AddEntityToModeSpace(line1,line2,line3);
+        }
+        [CommandMethod("TestLine2")]
+        public void TestLine2()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            //db.AddLineToModeSpace(new Point3d(100, 100, 0), new Point3d(200, 100, 0));
+            //Otherwise
+            db.AddLineToModeSpace(new Point3d(200, 100, 0), 30, 35);
+            db.AddLineToModeSpace(new Point3d(200, 200, 0), 30, 35);
+        }
+        #endregion
+        #region Day4
+        /// <summary>
+        /// 画圆弧
+        /// </summary>
+        [CommandMethod("TestArc")]
+        public void TestArc()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            Point3d startPoint = new Point3d(100, 100, 0);
+            Point3d arcOnPoint = new Point3d(200, 200, 0);
+            Point3d endPoint = new Point3d(150, 100, 0);
+            CircularArc3d cArc = new CircularArc3d(startPoint, arcOnPoint, endPoint);
+
+            double radius = cArc.Radius;
+            Point3d center = cArc.Center;
+            Vector3d cs = center.GetVectorTo(startPoint);
+            Vector3d ce = center.GetVectorTo(endPoint);
+            Vector3d xVector = new Vector3d(1, 0, 0);
+            double startAngle = cs.Y > 0 ? cs.GetAngleTo(xVector) : -xVector.GetAngleTo(cs);
+            double endAngle =ce.Y > 0 ? xVector.GetAngleTo(ce) : -xVector.GetAngleTo(ce);
+            Arc arc = new Arc(center, radius, startAngle, endAngle);
+
+            db.AddEntityToModeSpace(arc);
+        }
+        /// <summary>
+        /// 画圆
+        /// </summary>
+        [CommandMethod("TestCircle")]
+        public void TestCircle()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            //圆心画圆
+            db.AddCircleToModeSpace(new Point3d(100, 100, 0), 100);
+            //两点画圆
+            db.AddCircleToModeSpace(new Point3d(200, 100, 0), new Point3d(300, 100, 0));
+            //三点画圆
+            db.AddCircleToModeSpace(new Point3d(400, 100, 0), new Point3d(600, 100, 0), new Point3d(600, 100, 0));
+        }
+        /// <summary>
+        /// 多线段
+        /// </summary>
+        [CommandMethod("TestPolyLine")]
+        public void TestPolyLine()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            //绘制多线段
+            db.AddPolyLineToModeSpace(false, 0, new Point2d(10, 10), new Point2d(50, 10), new Point2d(100, 20)); //参数： 是否闭合 线宽 两个平面点
+            db.AddPolyLineToModeSpace(true, 0, new Point2d(10, 50), new Point2d(50, 50), new Point2d(100, 70));
+            //绘制矩形
+            db.AddRectToModeSpace(new Point2d(0, 0), new Point2d(100, 100));
+            db.AddRectToModeSpace(new Point2d(200, 200), new Point2d(100, 100));
+            db.AddRectToModeSpace(new Point2d(500, 500), new Point2d(100, 100));
+
+            //绘制正多边形
+            db.AddPolygonToModeSpace(new Point2d(100, 100), 50, 3, 90);
+            db.AddPolygonToModeSpace(new Point2d(200, 100), 50, 4, 45);
+            db.AddPolygonToModeSpace(new Point2d(300, 100), 50, 5, 90);
+            db.AddPolygonToModeSpace(new Point2d(400, 100), 50, 6, 0);
+            db.AddPolygonToModeSpace(new Point2d(500, 100), 50, 12, 0);
+        }
+        /// <summary>
+        /// 椭圆
+        /// </summary>
+        [CommandMethod("TestEllipse")]
+        public void TestEllipse()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            //绘制椭圆
+            db.AddEllipseToModeSpace(new Point3d(20, 20, 0), new Point3d(200, 200, 0),60);
+            db.AddEllipseToModeSpace(new Point3d(100, 100, 0), new Point3d(500, 500, 0));
+        }
+        #endregion
+    }
+}
